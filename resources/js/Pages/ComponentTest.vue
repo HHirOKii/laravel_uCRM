ecea9364843ed14b378ec8b368b4c804a97bf107
HHirOKii <narihiro1707@gmail.com>
2024-10-11 21:49:12 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import GuestLayout from '@/Layouts/GuestLayout.vue';
+    import Label from '@/Components/InputLabel.vue';
+    import InputText from '@/Components/TextInput.vue';
+
+    const emitTest = e => console.log(e);
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名">
+            タイトル
+        </Label>
+        <InputText modelValue=初期値が入ります
+        @update:modelValue="emitTest"
+        ></InputText>
+    </GuestLayout>
+</template>
