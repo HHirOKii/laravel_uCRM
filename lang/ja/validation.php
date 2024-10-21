1e841a3a81690e8729915b99fe26e01ddfe68551
HHirOKii <narihiro1707@gmail.com>
2024-10-21 10:15:52 +0900

    customers

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -163,7 +163,14 @@ return [
       'contact' => 'お問い合わせ内容',
       'caution' => '注意事項',
       'memo' => 'メモ',
-      'price' => '商品価格'
+      'price' => '商品価格',
+      'kana' => 'カナ',
+      'tel' => '電話番号',
+      'email' => 'メールアドレス',
+      'postcode' => '郵便番号',
+      'address' => '住所',
+      'birthday' => '誕生日',
+      'gender' => '性別'
     ],
 
 ];
